2f58dea4bcc0b9c5df89f514903207e5c6acd261
Leon <leo.muscle28@gmail.com>
2024-09-29 17:10:09 +0100

    added: menu

--- src/Main.kt
+++ src/Main.kt
@@ -19,69 +19,9 @@ fun main() {
     }
 
 }
-
 fun basicCalc(scanner: Scanner) {
-    println("Будь-ласка, виберіть операцію(+, -, *, /): ")
-    val operator = scanner.next()
 
-    println("Введіть перше значення:")
-    val num1 = scanner.nextDouble()
-
-    println("Введіть друге значення:")
-    val num2 = scanner.nextDouble()
-
-    val result = when (operator) {
-        "+" -> num1 + num2
-        "-" -> num1 - num2
-        "*" -> num1 * num2
-        "/" -> {
-            if (num2 != 0.0) {
-                num1 / num2
-            } else {
-                println("Ваше друге значення дорівнює нулю. Заборонено ділити на нуль")
-                println("Будь-ласка, введіть інше значення та спробуйте ще раз.")
-                return
-            }
-        }
-
-        else -> {
-            println("Був обраний неправильний оператор. Спробуйте ще раз ")
-            return
-        }
-    }
-
-    println("Результат $num1 $operator $num2 дорівнює: $result")
 }
-
 fun calcTriangle(scanner: Scanner) {
-    println("Введіть значення довжин трьох сторін трикутника:")
 
-    print("Перша сторона: ")
-    val a = scanner.nextDouble()
-
-    print("Друга сторона: ")
-    val b = scanner.nextDouble()
-
-    print("Третя сторона: ")
-    val c = scanner.nextDouble()
-
-    if (!isValidTriangle(a, b, c)) {
-        println("Сторони таких довжин не формують правильний трикутник.")
-        return
-    }
-
-    // обчислення півпериметра
-    val s = (a + b + c) / 2
-
-    println("Введіть радіус вписаного кола:")
-    val r = scanner.nextDouble()
-
-    // обчислення площи через формулу: Площа = r * s
-    val area = r * s
-
-    println("Площа трикутника зі сторонами $a, $b, $c і радіусом вписаного кола $r дорівнює: $area")
-}
-
-fun isValidTriangle(a: Double, b: Double, c: Double): Boolean {
-    return (a + b > c) && (a + c > b) && (b + c > a)
 }
