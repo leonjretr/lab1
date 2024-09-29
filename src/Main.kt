3913e8d6e8e6e72486a5ff9fe53f48e1e3d448bb
Leon <leo.muscle28@gmail.com>
2024-09-29 15:12:47 +0100

    second commit: created console greeting

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("")
+    println("Welcome to our Kotlin calculator")
+    println("It was made by Leonid Svietlychnyi and Roman Bak")
+    println("Please, choose the operation(+, -, *, /): ")
 }
