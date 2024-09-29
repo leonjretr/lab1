9495cded1c9ec78d2299416219a8e7f53c28965a
Leon <leo.muscle28@gmail.com>
2024-09-29 16:58:02 +0100

    added: calculation of the area of the triangle through the radius of the inscribed circle

--- src/Main.kt
+++ src/Main.kt
@@ -9,25 +9,25 @@ fun main() {
     println("Welcome to our Kotlin calculator")
     println("It was made by Leonid Svietlychnyi and Roman Bak")
 
-    println("Choose an option:")
-    println("1. Basic arithmetic operations")
-    println("2. Calculate area of a triangle through the radius of the inscribed circle")
+    println("Оберіть опцію:")
+    println("1. Базові арифметичні операції")
+    println("2. Розрахунок площини трикутника через радіус вписаної окружності")
     when (scanner.nextInt()) {
         1 -> basicCalc(scanner)
         2 -> calcTriangle(scanner)
-        else -> println("Invalid option.")
+        else -> println("Неправильна опція. Спробуйте ще раз")
     }
 
 }
 
-fun basicCalc(scanner:Scanner) {
-    println("Please, choose the operation(+, -, *, /): ")
+fun basicCalc(scanner: Scanner) {
+    println("Будь-ласка, виберіть операцію(+, -, *, /): ")
     val operator = scanner.next()
 
-    println("Enter first value:")
+    println("Введіть перше значення:")
     val num1 = scanner.nextDouble()
 
-    println("Enter second value:")
+    println("Введіть друге значення:")
     val num2 = scanner.nextDouble()
 
     val result = when (operator) {
@@ -38,20 +38,50 @@ fun basicCalc(scanner:Scanner) {
             if (num2 != 0.0) {
                 num1 / num2
             } else {
-                println("Your second value is 0. It's forbidden to divide by 0")
-                println("Please, choose another number")
+                println("Ваше друге значення дорівнює нулю. Заборонено ділити на нуль")
+                println("Будь-ласка, введіть інше значення та спробуйте ще раз.")
                 return
             }
         }
 
         else -> {
-            println("Invalid operator. Try again")
+            println("Був обраний неправильний оператор. Спробуйте ще раз ")
             return
         }
     }
 
-    println("The result of $num1 $operator $num2 is: $result")
+    println("Результат $num1 $operator $num2 дорівнює: $result")
 }
-fun calcTriangle(scanner:Scanner){
 
+fun calcTriangle(scanner: Scanner) {
+    println("Введіть значення довжин трьох сторін трикутника:")
+
+    print("Перша сторона: ")
+    val a = scanner.nextDouble()
+
+    print("Друга сторона: ")
+    val b = scanner.nextDouble()
+
+    print("Третя сторона: ")
+    val c = scanner.nextDouble()
+
+    if (!isValidTriangle(a, b, c)) {
+        println("Сторони таких довжин не формують правильний трикутник.")
+        return
+    }
+
+    // обчислення півпериметра
+    val s = (a + b + c) / 2
+
+    println("Введіть радіус вписаного кола:")
+    val r = scanner.nextDouble()
+
+    // обчислення площи через формулу: Площа = r * s
+    val area = r * s
+
+    println("Площа трикутника зі сторонами $a, $b, $c і радіусом вписаного кола $r дорівнює: $area")
+}
+
+fun isValidTriangle(a: Double, b: Double, c: Double): Boolean {
+    return (a + b > c) && (a + c > b) && (b + c > a)
 }
