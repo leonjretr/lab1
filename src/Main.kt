13543a369c223a24eeca142225c83b5d47d1eee2
Leon <leo.muscle28@gmail.com>
2024-09-29 15:16:21 +0100

    third commit: created console input for operation value and 1st number

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,21 @@
+import java.util.Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
+    val scanner = Scanner(System.`in`)
+
     println("Welcome to our Kotlin calculator")
     println("It was made by Leonid Svietlychnyi and Roman Bak")
+
     println("Please, choose the operation(+, -, *, /): ")
+    val operator = scanner.next()
+
+    println("Enter first value:")
+    val num1 = scanner.nextDouble()
+
+
+
+
+
 }
